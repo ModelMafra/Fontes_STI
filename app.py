436be7e00e6b5457f30791b7fcd97cb90fa0fb04
ModelMafra <100168694+ModelMafra@users.noreply.github.com>
2022-04-08 15:39:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,27 @@ import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
 import plotly as pl
-
+import warnings
+warnings.filterwarnings('ignore')
 
 df = pd.read_excel("tabela produtos.xlsx")
 
-mark_values= {2010:'2010', 2011:'2011', 2012:'2012', 2013:'2013', 2014:'2014',
-             2015:'2015', 2016:'2016', 2017:'2017', 2018:'2018',
-             2019:'2019', 2020:'2020', 2021:'2021', 2022:'2022'}
+mark_values= {2010:{'label':'2010', 'style': {'font-size': '140%'}},
+2011:{'label':'2011', 'style': {'font-size': '140%'}},
+2012:{'label':'2012', 'style': {'font-size': '140%'}},
+2013:{'label':'2013', 'style': {'font-size': '140%'}},
+2014:{'label':'2014', 'style': {'font-size': '140%'}},
+2015:{'label':'2015', 'style': {'font-size': '140%'}},
+2016:{'label':'2016', 'style': {'font-size': '140%'}},
+2017:{'label':'2017', 'style': {'font-size': '140%'}},
+2018:{'label':'2018', 'style': {'font-size': '140%'}},
+2019:{'label':'2018', 'style': {'font-size': '140%'}},
+2020:{'label':'2020', 'style': {'font-size': '140%'}},
+2021:{'label':'2021', 'style': {'font-size': '140%'}},
+2022:{'label':'2022', 'style': {'font-size': '140%'}}}
+
+
+
 app = Dash(__name__)
 server = app.server
 
@@ -32,9 +46,9 @@ fig = px.sunburst(df, path=['Fonte', 'Área', 'Produto'],
                                       "Google Analytics": "#D5F2DA",
                                       "SIMPPO": "#BBDBB6",
                                       "VagasNaPraia": "#E4F9CF"},
-                  height=950,
-
+                  height=950
                   )
+fig.update_layout(uniformtext=dict(minsize=14))
 # ------------------------------------------------------------------------------
 # App layout
 app.layout = html.Div([
@@ -42,19 +56,25 @@ app.layout = html.Div([
         html.H1("Sistemas e Indicadores de Apoio à Decisão STI ",
                 style={'backgroundColor': colors['background'],
                        'text-align': 'center',
-                      'color': colors['text'],
+                       'color': colors['text'],
                        'font-family': 'Arial'}),
     html.Br(),
 
     html.Div([
         dcc.Tabs(id='tabs-example-1', value='tab-1', children=[
-        dcc.Tab(label='Fontes por data', value='tab-1'),
-        dcc.Tab(label='Fontes por àrea e Produto', value='tab-2'),
-            ])
+        dcc.Tab(label='Fontes por data',
+                value='tab-1',
+                style={'font-size': '140%', 'font-family': 'Arial', 'color': '#bbbdbf'},
+                selected_style={'font-size': '140%', 'font-family': 'Arial'}),
+
+        dcc.Tab(label='Produtos e Área por Fonte',
+                value='tab-2',
+                style={'font-size': '140%', 'font-family': 'Arial', 'color': '#bbbdbf'},
+                selected_style={'font-size': '140%', 'font-family': 'Arial'})
         ]),
     html.Div(id='tabs-example-content-1')
+            ]),
     ]),
-
 ])
 
 @app.callback(
@@ -78,9 +98,10 @@ def render_content(tab):
     elif tab == 'tab-2':
           return html.Div([dcc.Graph(id="sunburst", figure=fig)])
 
+
 @app.callback(
-    Output('scatter-plot','figure'),
-    [Input('year-slider','value')]
+    Output('scatter-plot', 'figure'),
+    Input('year-slider', 'value')
     )
 def update_graph(years_chosen):
     # print(years_chosen)
